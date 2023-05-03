c4968e1ff33a874338787b98b2528bc928ab4162
JonathanGustafCilli <jonathan.gustaf.cilli@gmail.com>
2023-05-03 14:41:07 +0200

    Took away useless comments

--- Code/algorithms/snudda/td_snudda.py
+++ Code/algorithms/snudda/td_snudda.py
@@ -3,16 +3,7 @@ from plotter import Plot
 import numpy as np
 from algorithms.snudda.Hypervoxel import Hypervoxel
 import time
-'''
-SE_F = Cube(0, 0, 0, s)
-    SW_F = Cube(s, 0, 0, s)
-    NE_F = Cube(0, s, 0, s)
-    NW_F = Cube(s, s, 0, s)
-    SE_B = Cube(0, 0, s, s)
-    SW_B = Cube(s, 0, s, s)
-    NE_B = Cube(0, s, s, s)
-    NW_B = Cube(s, s, s, s)
-'''
+
 def touch_detection(neurons):
     
     start = time.time()
